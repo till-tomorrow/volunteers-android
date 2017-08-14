e0561b67e5c334422a5317c0dfff2dc9a7e7301e
Divya Pandilla <divya.pandilla8@gmail.com>
2017-08-14 14:51:53 +0530

    Schedule Page. (#27)
    * Events List
    
    * Manual Sign In with Username and Password.
    
    * Manual Sign In with Email and password
    
    * Navigate to HomePage on successful SignIn
    
    * Modified UI and removed hard coded strings
    
    * Made Suggested Changes.
    
    1. Added Utils Package and made the HttpHandler a util class
    2. Renamed Menu to MenuActivity
    3.  Removed HashMap and created a separate event class.
    4. Created a separate Custom Handler
    5. Changed names of variables to specify their purpose.
    6. Made Layout changes.
    
    * Schedule Page
    
    * Modified list view to card view
    
    * Added separate Adapter class and removed HashMap.

---
+++ app/src/main/java/vola/systers/com/volunteers_android/adapter/ScheduleEventsListAdapter.java
@@ -0,0 +1,62 @@
+package vola.systers.com.volunteers_android.adapter;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import vola.systers.com.volunteers_android.R;
+import vola.systers.com.volunteers_android.model.Event;
+
+public class ScheduleEventsListAdapter extends ArrayAdapter<Event>{
+
+    private ArrayList<Event> dataSet;
+    Context mContext;
+
+    // View lookup cache
+    private static class ViewHolder {
+        TextView eventName;
+        TextView date;
+        TextView time;
+        TextView location;
+    }
+
+    public ScheduleEventsListAdapter(ArrayList<Event> data, Context context) {
+        super(context, R.layout.schedule_list_item, data);
+        this.dataSet = data;
+        this.mContext=context;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Get the data item for this position
+        Event event = getItem(position);
+
+        // Check if an existing view is being reused, otherwise inflate the view
+        ViewHolder viewHolder; // view lookup cache stored in tag
+
+        if (convertView == null) {
+            viewHolder = new ViewHolder();
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.schedule_list_item, parent, false);
+            viewHolder.eventName = (TextView) convertView.findViewById(R.id.event_name);
+            viewHolder.date = (TextView) convertView.findViewById(R.id.date);
+            viewHolder.time = (TextView) convertView.findViewById(R.id.time);
+            viewHolder.location = (TextView) convertView.findViewById(R.id.location);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        viewHolder.eventName.setText(event.getName());
+        viewHolder.date.setText(event.getStartDate()+" to "+event.getEndDate());
+        viewHolder.time.setText(event.getStartTime()+" to "+event.getEndTime());
+        viewHolder.location.setText("Orlando");
+
+        // Return the completed view to render on screen
+        return convertView;
+    }
+}
--- app/src/main/java/vola/systers/com/volunteers_android/fragments/ScheduleFragment.java
+++ app/src/main/java/vola/systers/com/volunteers_android/fragments/ScheduleFragment.java
@@ -1,25 +1,141 @@
 package vola.systers.com.volunteers_android.fragments;
 
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.Toast;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.util.ArrayList;
 
 import vola.systers.com.volunteers_android.R;
+import vola.systers.com.volunteers_android.adapter.ScheduleEventsListAdapter;
+import vola.systers.com.volunteers_android.utils.HttpHandler;
+import vola.systers.com.volunteers_android.model.Event;
 
 public class ScheduleFragment extends Fragment {
 
-    public ScheduleFragment(){}
+
+    public ScheduleFragment() {
+    }
+
+    private String TAG = ScheduleFragment.class.getSimpleName();
+
+    private ProgressDialog pDialog;
+    private ListView eventsListView;
+    static String startDate,endDate,id,name,startTime,endTime;
+    private static ScheduleEventsListAdapter eventListAdapter;
+
+    // URL to get events JSON
+    private static String eventsURL = "http://divya-gsoc.esy.es/sample/data.json";
+    private static String eventDetailsURL = "http://divya-gsoc.esy.es/sample/data2.json";
+
+    ArrayList<Event> eventList;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        // TODO : SCHEDULE FRAGMENT
-
-        View rootView = inflater.inflate(R.layout.profile_fragment, container, false);
+        View rootView = inflater.inflate(R.layout.schedule_list_fragment, container, false);
+        eventList = new ArrayList<>();
+        eventsListView = (ListView) rootView.findViewById(R.id.list);
+        new GetEvents().execute();
         return rootView;
+    }
+
+    /**
+     * Async task class to get json by making HTTP call
+     */
+    private class GetEvents extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            // Showing progress dialog
+            pDialog = new ProgressDialog(getActivity());
+            pDialog.setMessage("Please wait...");
+            pDialog.setCancelable(false);
+            pDialog.show();
+
+        }
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            // Making a request to url and getting response
+            String eventsJsonStr = HttpHandler.makeServiceCall(eventsURL);
+            String eventDetailsJsonStr = HttpHandler.makeServiceCall(eventDetailsURL);
+
+            if (eventsJsonStr != null && eventDetailsJsonStr != null) {
+                try {
+                    JSONObject eventsJsonObj = new JSONObject(eventsJsonStr);
+                    JSONObject eventDetailsJsonObject = new JSONObject(eventDetailsJsonStr);
+
+                    // Getting JSON Array node
+                    JSONArray events = eventsJsonObj.getJSONArray("events");
+
+                    // looping through All events
+                    for (int i = 0; i < events.length(); i++) {
+                        JSONObject eventJs = events.getJSONObject(i);
+
+                        id = eventJs.getString("eventid");
+                        name = eventJs.getString("name");
+                        startDate = eventDetailsJsonObject.getString("startdate");
+                        endDate = eventDetailsJsonObject.getString("enddate");
+                        startTime = eventDetailsJsonObject.getString("starttime");
+                        endTime=eventDetailsJsonObject.getString("endtime");
+
+                        eventList.add(new Event(id, name, startDate,endDate,startTime,endTime));
+                    }
+                } catch (final JSONException e) {
+                    Log.e(TAG, "Json parsing error: " + e.getMessage());
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getContext(),
+                                    "Json parsing error: " + e.getMessage(),
+                                    Toast.LENGTH_LONG)
+                                    .show();
+                        }
+                    });
+
+                }
+            } else {
+                Log.e(TAG, "Couldn't get json from server.");
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getContext(),
+                                "Couldn't get json from server. Check LogCat for possible errors!",
+                                Toast.LENGTH_LONG)
+                                .show();
+                    }
+                });
+
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+
+            // Dismiss the progress dialog
+            if (pDialog.isShowing())
+                pDialog.dismiss();
+
+            eventListAdapter = new ScheduleEventsListAdapter(eventList,getContext());
+            eventsListView.setAdapter(eventListAdapter);
+        }
+
     }
 
 }

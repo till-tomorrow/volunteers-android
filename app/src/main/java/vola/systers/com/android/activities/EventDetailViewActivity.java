e9113a5679dce3bd91fb7005c50d524640e7a6b6
Divya Pandilla <divya.pandilla8@gmail.com>
2017-10-28 19:57:22 +0530

    Bookmarking events (after the user logs in to the application) (#82)
    * user can bookmark any event (after logging in to the application)
    
    * Navigate to Event Details Page if bookmarked event is clicked
    
    * bookmarking event only once.
    
    * Changed background color of FAB to green when event is starred.

--- app/src/main/java/vola/systers/com/android/activities/EventDetailViewActivity.java
+++ app/src/main/java/vola/systers/com/android/activities/EventDetailViewActivity.java
@@ -2,34 +2,49 @@ package vola.systers.com.android.activities;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.res.ColorStateList;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 import vola.systers.com.android.R;
 import vola.systers.com.android.model.Event;
 import vola.systers.com.android.utils.NetworkConnectivity;
 
+import static android.content.ContentValues.TAG;
+
 public class EventDetailViewActivity extends AppCompatActivity implements View.OnClickListener{
 
     private TextView eventName,eventDescription,locationName,locationCity,locationCountry,eventTime,eventDate;
     private Button register;
+    public static String userToken="",eventId="";
     private CoordinatorLayout coordinatorLayout;
     final static FirebaseDatabase database = FirebaseDatabase.getInstance();
+    public ArrayList starredEvents = new ArrayList();
+    private FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +52,7 @@ public class EventDetailViewActivity extends AppCompatActivity implements View.O
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_detail_view);
         coordinatorLayout = (CoordinatorLayout) findViewById(R.id.coordinator_layout);
+        fab = (FloatingActionButton) findViewById(R.id.fab);
 
         if(! new NetworkConnectivity().checkConnectivity(this)) {
             Snackbar snackbar = Snackbar
@@ -45,6 +61,32 @@ public class EventDetailViewActivity extends AppCompatActivity implements View.O
             sbView.setBackgroundColor(ContextCompat.getColor(this, R.color.colorPrimaryDark));
             snackbar.show();
         }
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            userToken = user.getUid();
+        }
+        DatabaseReference starredEventsRef = database.getReference("starred_events").child(userToken);
+        ValueEventListener valueEventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                    starredEvents.add(ds.getKey().toString());
+                }
+                if(starredEvents.contains(eventId)){
+                    Log.i("EVENT IDS", eventId);
+                    fab.setAlpha(.5f);
+                    fab.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.emerald)));
+                    fab.setClickable(false);
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.w(TAG, "Failed to read value.", databaseError.toException());
+            }
+
+        };
+        starredEventsRef.addValueEventListener(valueEventListener);
 
         Event event = (Event) getIntent().getSerializableExtra("selectedEvent");
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -65,6 +107,7 @@ public class EventDetailViewActivity extends AppCompatActivity implements View.O
         String time = "TIME : " + event.getStartTime()+" to "+event.getEndTime();
 
         eventName = (TextView) findViewById(R.id.event_name);
+        eventId= event.getId();
         eventDescription = (TextView)findViewById(R.id.event_description);
         locationName = (TextView) findViewById(R.id.event_location_name);
         locationCity = (TextView)findViewById(R.id.event_location_city);
@@ -73,6 +116,7 @@ public class EventDetailViewActivity extends AppCompatActivity implements View.O
         eventDate=(TextView)findViewById(R.id.event_date);
         register=(Button)findViewById(R.id.btn_register);
         register.setOnClickListener(this);
+        fab.setOnClickListener(this);
 
         eventName.setText(name);
         eventDescription.setText(description);
@@ -81,11 +125,13 @@ public class EventDetailViewActivity extends AppCompatActivity implements View.O
         locationCountry.setText(country);
         eventDate.setText(date);
         eventTime.setText(time);
+
         if(!event.getStatus().equals("Require Volunteers") && !event.getStatus().equals(""))
         {
             register.setAlpha(.5f);
             register.setClickable(false);
         }
+
     }
 
     @Override
@@ -108,6 +154,7 @@ public class EventDetailViewActivity extends AppCompatActivity implements View.O
             snackbar.show();
         }
         else {
+
             FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
             if (user != null) {
                 Event event = (Event) getIntent().getSerializableExtra("selectedEvent");
@@ -141,12 +188,42 @@ public class EventDetailViewActivity extends AppCompatActivity implements View.O
         }
     }
 
+    private void bookMarkEvent()
+    {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        if(! new NetworkConnectivity().checkConnectivity(this)) {
+            Snackbar snackbar = Snackbar
+                    .make(coordinatorLayout, "Please Make Sure You are Connected to Internet!", Snackbar.LENGTH_LONG);
+            View sbView = snackbar.getView();
+            sbView.setBackgroundColor(ContextCompat.getColor(this, R.color.colorPrimaryDark));
+            snackbar.show();
+        }
+        else {
+            if (user!=null) {
+
+                userToken = user.getUid();
+                DatabaseReference eventsRef = database.getReference("starred_events");
+                eventsRef.child(userToken).child(eventId).child("bookmarked").setValue("true");
+
+                Snackbar snackbar = Snackbar
+                        .make(coordinatorLayout, "BookMarked Successfull!", Snackbar.LENGTH_LONG);
+                View sbView = snackbar.getView();
+                sbView.setBackgroundColor(ContextCompat.getColor(this, R.color.colorPrimaryDark));
+                snackbar.show();
+            }
+          }
+    }
+
     @Override
     public void onClick(View v) {
         int id = v.getId();
         switch (id) {
             case R.id.btn_register:
                 registerEvent();
+                break;
+            case R.id.fab:
+                bookMarkEvent();
                 break;
         }
     }
